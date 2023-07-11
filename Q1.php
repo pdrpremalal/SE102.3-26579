3e29bb9b529c98522c5d3c97eb2bb2a144632c97
pdrpremalal <138178712+pdrpremalal@users.noreply.github.com>
2023-07-11 08:08:50 -0700

    Add files via upload

---
+++ Q1.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+<?php
+$x=10;
+$y=20;
+
+echo "sum: ".$x + $y."<br>";
+echo "sub: ".$x - $y."<br>";
+echo "mul: ".$x * $y."<br>";
+echo "div: ".$x / $y."<br>";
+
+?>
+</body>
+</html>
